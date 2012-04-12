5aab9dfb5656b60bd40663dac7eaccadc2bc0b24
rahul patil <rahul.patil3@synechron.com>
2012-04-12 15:12:28 +0530

    olms-init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProApp::Application.config.secret_token = 'ed0617f2cc6b108ba6e3d281b95f3c9a835a3c0c955478abe5ee3c2da39295ded6b3b8e8d2e2ab7ec9eb8d08f840237f7846176063fa3b4d0a990589f5c99471'
